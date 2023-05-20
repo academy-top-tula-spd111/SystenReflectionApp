6f10debe4bd5fbe9a36582ef96461f9fe9fa37b2
maxefimov <efimov.top.academy@gmail.com>
2023-05-20 17:03:47 +0300

    Добавьте файлы проекта.

---
+++ SystenReflectionApp/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystenReflectionApp
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    interface ISleeper
+    {
+        void Sleep();
+    }
+    internal class Person : IMovable, ISleeper
+    {
+        int var;
+        public string Name { get; set; } = "";
+        public int Age { set; get; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+
+        public void Print(int a)
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+
+        void PrintPrivate()
+        {
+            Console.WriteLine($"Private. Name: {Name}, Age: {Age}");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Move");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Sleep");
+        }
+
+        public override string ToString()
+        {
+            return $"Private. Name: {Name}, Age: {Age}";
+        }
+
+        virtual public void Send(out double x, in string s, FileInfo info)
+        {
+            x = 100.0;
+        }
+
+        public Action action;
+    }
+}
---
+++ SystenReflectionApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using SystenReflectionApp;
+
+Person person = new("Bob", 32);
+
+Type myType = person.GetType();
+Console.WriteLine(myType);
+
+Console.WriteLine($"Name: {myType.Name}");
+Console.WriteLine($"Full Name: {myType.FullName}");
+Console.WriteLine($"Namespace: {myType.Namespace}");
+Console.WriteLine($"Is struct: {myType.IsValueType}");
+Console.WriteLine($"Is class: {myType.IsClass}");
+
+Console.WriteLine("\nMembers:");
+foreach (var t in myType.GetMembers(BindingFlags.Instance | BindingFlags.NonPublic))
+    Console.WriteLine($"\t{t.MemberType} {t.DeclaringType} {t.Name}");
+
+Console.WriteLine("\nIntefaces:");
+foreach (var t in myType.GetInterfaces())
+    Console.WriteLine($"\t{t.Name}");
+
+Console.WriteLine("\nMethods");
+foreach(var t in myType.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+{
+    string str = $"{t.Name}: ";
+    str += t.IsConstructor ? " construct" : " non construct";
+    str += t.IsVirtual ? " virtual" : " non virtual";
+    str += t.IsPublic ? " public" : "";
+    str += t.IsPrivate ? " private" : "";
+
+    Console.WriteLine($"\t{str}");
+
+    foreach(var p in t.GetParameters())
+    {
+        string pstr = $"{p.Name}: ";
+        pstr += p.ParameterType.Name;
+        pstr += p.IsOut ? " Out" : "";
+        pstr += p.IsIn ? " In" : "";
+
+        Console.WriteLine($"\t\t{pstr}");
+    }
+    if(t.GetParameters().Length == 0)
+    {
+        t?.Invoke(person, parameters: null);
+    }
+        
+}
+    
+
+Console.WriteLine("\nMethods Print");
+foreach (var t in myType.GetMember("Print", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic ))
+    Console.WriteLine($"\t{t.Name}");
+
+
